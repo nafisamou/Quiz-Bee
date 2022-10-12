40528356bd388f13037eab7521f2d748ee1783dc
nafisamou <108416863+nafisamou@users.noreply.github.com>
2022-10-12 21:56:13 +0600

    responsive

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -10,7 +10,7 @@ const CardDetails = () => {
         <h2 className="text-2xl mb-2 flex justify-center underline font-semibold font-serif p-12"> Quize of <span className="text-cyan-700 ml-3 font-serif">{name} </span></h2>
         <h3 className="font-mono text-xl">Total Quize : <span className="font-semibold">{total}</span></h3>
       </div>
-      <div className="grid lg:grid-cols-1 gap-4 grid-cols-1 px-52">
+      <div className="grid lg:grid-cols-1 gap-4  lg:px-52">
         {questions.map((questionArray) => (
           <Quiz key={questionArray.id} questionArray={questionArray}></Quiz>
         ))}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -28,7 +28,7 @@ const Quiz = ({ questionArray }) => {
         </button>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-4 border-4 p-4 bg-white shadow-lg">
+      <div className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-4 border-4 p-4 border-1 bg-white shadow-lg">
         {options.map((option) => (
           <Options option={option} handleClick={handleClick}></Options>
         ))}
